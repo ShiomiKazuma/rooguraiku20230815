46fc495c581c844ed6d7a40cd481ef56f389b85f
ShiomiKazuma <134474097+ShiomiKazuma@users.noreply.github.com>
2023-09-02 22:52:30 +0900

    [add]アイテムベース、マップジェネレータの追加

---
+++ Assets/Script/ItemBase.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ItemBase : MonoBehaviour
+{
+    [SerializeField] ActiveTiming _timing = ActiveTiming.Get;
+    public abstract void Activate();
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (_timing == ActiveTiming.Get)
+        {
+            Activate();
+            Destroy(this.gameObject);
+        }
+        else if(_timing == ActiveTiming.Use)
+        {
+            //アイテムボックスに移動する処理
+        }
+    }
+    enum ActiveTiming
+    {
+        Get,
+        Use,
+    }
+}
