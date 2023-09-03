20c77f8ef4d393a9c6760b24ef19b67f444213d1
ShiomiKazuma <134474097+ShiomiKazuma@users.noreply.github.com>
2023-09-03 23:37:04 +0900

    [add]プレイヤーステータス機能を追加

---
+++ Assets/Script/PlayerStatas.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStatas : MonoBehaviour
+{
+    [SerializeField] int _hp;
+    [SerializeField] int _sutamina;
+    [SerializeField] int _power;
+    st statas = st.Normal;
+    
+    public enum st
+    {
+        Normal,
+        Poison, 
+    }
+    public void Damage(int damage)
+    {
+        _hp -= damage;
+    }
+
+    public void SutaminaDown(int down)
+    {
+        _sutamina -= down;
+    }
+}
