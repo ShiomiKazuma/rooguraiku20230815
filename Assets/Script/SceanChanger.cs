ad13119a2a64d3093017eb3e86dd832f142a92fc
ShiomiKazuma <134474097+ShiomiKazuma@users.noreply.github.com>
2023-09-04 15:20:51 +0900

    [add]シーンチェンジャーを追加

---
+++ Assets/Script/SceanChanger.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceanChanger : MonoBehaviour
+{
+    [SerializeField] Image _panel;
+    public void SceanChange()
+    {
+        _panel.gameObject.SetActive(true);
+        _panel.DOFade(1, 3.0f).OnComplete(() => SceneManager.LoadScene("hoge"));
+    }
+}
