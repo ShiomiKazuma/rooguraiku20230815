d2d4334542375171f8f7a95a37067a4e54bae6a1
ShiomiKazuma <134474097+ShiomiKazuma@users.noreply.github.com>
2023-09-08 22:46:41 +0900

    [add]メッセージウィンドウの処理を追加

---
+++ Assets/Script/MessageWindow.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MessageWindow : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI _dialogText;
+    [TextArea(5, 5)]
+    [SerializeField] string _msgText;
+    [SerializeField] float _msgSpeed = 0.01f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _dialogText.text = "";
+        
+    }
+
+    IEnumerable TypeDisplay()
+    {
+        foreach(char item in _msgText.ToCharArray())
+        {
+            _dialogText.text += item;
+            yield return new WaitForSeconds(_msgSpeed);
+        }
+    }
+}
