a1108a9eea1700bf351c0be0d788458ea74d0ad3
ShiomiKazuma <134474097+ShiomiKazuma@users.noreply.github.com>
2023-09-10 12:54:19 +0900

    [add]SEマネージャーを追加

---
+++ Assets/Script/SEManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SEManager : MonoBehaviour
+{
+    [SerializeField] AudioSource _audioSource;
+    [SerializeField, Header("決定するときに鳴らす音")] AudioClip _select;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    public void SelectSound()
+    {
+        _audioSource.PlayOneShot(_select);
+    }
+
+    /// <summary>
+	/// スライドバー値の変更イベント
+	/// </summary>
+	/// <param name="newSliderValue">スライドバーの値(自動的に引数に値が入る)</param>
+	public void SoundSliderOnValueChange(float newSliderValue)
+    {
+        // 音楽の音量をスライドバーの値に変更
+        _audioSource.volume = newSliderValue;
+    }
+}
