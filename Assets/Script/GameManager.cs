05b894ec0895fdea415a138dbafb1125195b8111
ShiomiKazuma <134474097+ShiomiKazuma@users.noreply.github.com>
2023-09-14 21:31:35 +0900

    [fix]ゲームマネージャーとプレイヤーステータスの処理の修正

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,20 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public float levelStartDelay = 2f;
     public float turnDelay = 0.1f;
     public static GameManager _instance;
     MapCreate _mapCreate;
     public static int _floor = 1;
-    [SerializeField] int _level = 3;
+    public static int _level = 3;
     List<Enemy> _enemies;
     bool enemiesMoving;
 
     [SerializeField] int _maxFoodPoint = 100;
     public int _foodPoint = 100;
     [HideInInspector] public bool _playersTurn = true;
+
+    Text _levelText;
+    GameObject _levelImage;
+    bool doingSetup;
 
     private void Awake()
     {
@@ -28,14 +36,40 @@ public class GameManager : MonoBehaviour
         }
 
         DontDestroyOnLoad(gameObject);
+        //_enemies = new List<Enemy>();
         _mapCreate = GetComponent<MapCreate>();
         InitGame();
         _foodPoint = _maxFoodPoint;
     }
 
+    private void Start()
+    {
+        SceneManager.sceneLoaded += SceneLoaded;
+    }
     public void InitGame()
     {
+        //ロード中の背景を表示
+        doingSetup = true;
+        _levelImage = GameObject.Find("LevelImage");
+        _levelText = GameObject.Find("LevelText").GetComponent<Text>();
+        _levelText.text = "Day" + _level;
+        _levelImage.SetActive(true);
+        Invoke("HideLevelImage", levelStartDelay);
+        //_enemies.Clear();
+        //マップの生成
         _mapCreate.SetUpScean(_level);
+    }
+
+    void HideLevelImage()
+    {
+        _levelImage.SetActive(false);
+        doingSetup = false;
+    }
+
+    private void SceneLoaded(Scene scene, LoadSceneMode sceneMode)
+    {
+        _level++;
+        InitGame();
     }
 
     public void FoodRecover(int food)
@@ -49,7 +83,42 @@ public class GameManager : MonoBehaviour
 
     public void GameOver()
     {
+        _levelText.text = "After" + _level + "days, you starved.";
+        _levelImage.SetActive(true);
         enabled = false;
     }
 
+    private void Update()
+    {
+        //if(_playersTurn || enemiesMoving)
+        //{
+        //    return;
+        //}
+
+        //StartCoroutine(MoveEnemies());
+
+    }
+
+    //public void AddEnemyToList(Enemy script)
+    //{
+    //    _enemies.Add(script);
+    //}
+    //IEnumerator MoveEnemies()
+    //{
+    //    enemiesMoving = true;
+    //    yield return new WaitForSeconds(turnDelay);
+    //    if(_enemies.Count == 0)
+    //    {
+    //        yield return new WaitForSeconds(turnDelay);
+    //    }
+
+    //    for (int i = 0; i < _enemies.Count; i++)
+    //    {
+    //        _enemies[i].MoveEnemy();
+    //        yield return new WaitForSeconds(_enemies[i].moveTime);
+    //    }
+
+    //    _playersTurn = true;
+    //    enemiesMoving = false;
+    //}
 }
--- Assets/Script/PlayerStatas.cs
+++ Assets/Script/PlayerStatas.cs
@@ -1,26 +1,110 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerStatas : MonoBehaviour
 {
-    [SerializeField] int _hp;
-    [SerializeField] int _sutamina;
+    [SerializeField] public int _hp;
     [SerializeField] int _power;
+    [SerializeField] public int _maxHp = 100;
     st statas = st.Normal;
-    
+    public int _wallDamage = 1;
+    public int _pointsPerFood = 10;
+    public int _pointsPerSoda = 20;
+    public float _restartLevelDelay = 1f;
+
+    Animator _animator;
+    int _food;
+    public Text _foodText;
     public enum st
     {
         Normal,
         Poison, 
     }
+
     public void Damage(int damage)
     {
         _hp -= damage;
     }
 
-    public void SutaminaDown(int down)
+    void Start()
     {
-        _sutamina -= down;
+        _animator = GetComponent<Animator>();
+        _food = GameManager._instance._foodPoint;
+        _foodText.text = "Food: " + _food;
+       // base.Start();
+    }
+
+    private void Update()
+    {
+
+    }
+    private void OnDisable()
+    {
+        GameManager._instance._foodPoint = _food;
+    }
+
+    //protected override void AttempMove<T>(int xDir, int yDir)
+    //{
+    //    _food--;
+    //    base.AttempMove<T>(xDir, yDir);
+    //    RaycastHit2D hit;
+    //    CheckGameOver();
+    //    GameManager._instance._playersTurn = false;
+    //}
+    public void CheckGameOver()
+    {
+        if(_food <= 0)
+        {
+            GameManager._instance.GameOver();
+        }
+    }
+    //protected override void OnCanMove<T>(T component)
+    //{
+    //    Wall hitWall = component as Wall;
+    //    hitWall.DamageWall(_wallDamage);
+    //    _animator.SetTrigger("PlayerChop");
+    //}
+
+    private void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void LoseFood(int loss)
+    {
+        _animator.SetTrigger("PlayerHit");
+        _food -= loss;
+        _foodText.text = "Food: " + _food;
+        CheckGameOver() ;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Exit")
+        {
+            Invoke("Restart", _restartLevelDelay);
+            enabled = false;
+        }
+        else if(collision.gameObject.tag == "Food")
+        {
+            _food += _pointsPerFood;
+            _foodText.text = "Food: " + _food;
+            collision.gameObject.SetActive(false);
+        }
+        else if(collision.gameObject.tag == "Soda")
+        {
+            _food += _pointsPerSoda;
+            _foodText.text = "Food: " + _food;
+            collision.gameObject.SetActive(false);
+        }
+    }
+
+    public void FoodDes()
+    {
+        _food--;
+        _foodText.text = "Food: " + _food;
     }
 }
